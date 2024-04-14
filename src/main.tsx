a92a665709851434715b46172922952d06e39500
Johann Haslinger <heyjohn655@gmail.com>
2024-04-14 20:13:18 +0200

    setup lsc and tailwindcss with styled components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import {
+  LeanScopeClient,
+  LeanScopeClientApp,
+} from "@leanscope/api-client/node";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <LeanScopeClientApp leanScopeClient={new LeanScopeClient()}>
+      <App />
+    </LeanScopeClientApp>
+  </React.StrictMode>
+);
---
+++ src/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+
+  darkMode: "class",
+  theme: {
+    fontFamily: {},
+    extend: {},
+  },
+  plugins: [],
+};
