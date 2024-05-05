d0835fd4b341ea38fbdd18e4827836885162e7a2
Johann Haslinger <heyjohn655@gmail.com>
2024-05-05 21:58:25 +0200

    Optimized Description Generating

--- src/features/collection/components/schoolSubjects/AddTopicSheet.tsx
+++ src/features/collection/components/schoolSubjects/AddTopicSheet.tsx
@@ -1,4 +1,4 @@
-import  { useContext, useState } from "react";
+import { useContext, useState } from "react";
 import {
   CancelButton,
   FlexBox,
@@ -15,11 +15,7 @@ import { DataTypes, Stories } from "../../../../base/enums";
 import { useSelectedSchoolSubject } from "../../hooks/useSelectedSchoolSubject";
 import { LeanScopeClientContext } from "@leanscope/api-client/node";
 import { Entity } from "@leanscope/ecs-engine";
-import {
-  DescriptionFacet,
-  IdentifierFacet,
-  ParentFacet,
-} from "@leanscope/ecs-models";
+import { DescriptionFacet, IdentifierFacet, ParentFacet } from "@leanscope/ecs-models";
 import { v4 } from "uuid";
 import { DateAddedFacet, TitleFacet } from "../../../../app/AdditionalFacets";
 import { useSelectedLanguage } from "../../../../hooks/useSelectedLanguage";
@@ -37,43 +33,30 @@ const AddTopicSheet = () => {
   const { selectedLanguage } = useSelectedLanguage();
   const { userId } = useUserData();
 
-  const navigateBack = () =>
-    lsc.stories.transitTo(Stories.OBSERVING_SCHOOL_SUBJECT_STORY);
+  const navigateBack = () => lsc.stories.transitTo(Stories.OBSERVING_SCHOOL_SUBJECT_STORY);
 
   const addTopic = async () => {
     if (selectedSchoolSubjectId) {
       const topicId = v4();
       let topicDescription = description;
 
-      if (description === "") {
-        // const generatingDescriptionPrompt =
-        //   selectedLanguage === SupportedLanguages.DE
-        //     ? "Bitte schreibe einen sehr kurzen Beschreibungssatz zu folgendem Thema:" +
-        //       title
-        //     : "Please provide a short description for the topic:" + title;
-        const generatingDescriptionPrompt =
-          "Bitte schreibe einen sehr kurzen Beschreibungssatz zu folgendem Thema:" +
-          title;
-
-        topicDescription = await getCompletion(generatingDescriptionPrompt);
-        console.log(generatingDescriptionPrompt);
-      }
-
       const newTopicEntity = new Entity();
       lsc.engine.addEntity(newTopicEntity);
       newTopicEntity.add(new IdentifierFacet({ guid: topicId }));
-      newTopicEntity.add(
-        new ParentFacet({ parentId: selectedSchoolSubjectId })
-      );
-      newTopicEntity.add(
-        new DescriptionFacet({ description: topicDescription })
-      );
+      newTopicEntity.add(new ParentFacet({ parentId: selectedSchoolSubjectId }));
+      newTopicEntity.add(new DescriptionFacet({ description: topicDescription }));
       newTopicEntity.add(new TitleFacet({ title: title }));
-      newTopicEntity.add(
-        new DateAddedFacet({ dateAdded: new Date().toISOString() })
-      );
+      newTopicEntity.add(new DateAddedFacet({ dateAdded: new Date().toISOString() }));
       newTopicEntity.add(DataTypes.TOPIC);
       navigateBack();
+
+      if (description === "") {
+        const generatingDescriptionPrompt =
+          "Bitte schreibe einen sehr kurzen Beschreibungssatz zu folgendem Thema:" + title;
+
+        topicDescription = await getCompletion(generatingDescriptionPrompt);
+        newTopicEntity.add(new DescriptionFacet({ description: topicDescription }));
+      }
 
       const { error } = await supabaseClient.from("topics").insert([
         {
@@ -93,14 +76,8 @@ const AddTopicSheet = () => {
   return (
     <Sheet navigateBack={navigateBack} visible={isVisible}>
       <FlexBox>
-        <CancelButton onClick={navigateBack}>
-          {displayButtonTexts(selectedLanguage).cancel}
-        </CancelButton>
-        {title !== "" && (
-          <SaveButton onClick={addTopic}>
-            {displayButtonTexts(selectedLanguage).save}
-          </SaveButton>
-        )}
+        <CancelButton onClick={navigateBack}>{displayButtonTexts(selectedLanguage).cancel}</CancelButton>
+        {title !== "" && <SaveButton onClick={addTopic}>{displayButtonTexts(selectedLanguage).save}</SaveButton>}
       </FlexBox>
       <Spacer />
       <Section>
