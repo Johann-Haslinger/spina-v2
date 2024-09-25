162dd387f1b13bf19a3222713f89fa5b3b1a164d
Johann Haslinger <heyjohn655@gmail.com>
2024-09-25 07:46:47 +0200

    feat: improve file viewer

--- src/features/collection/components/learning_units/FileViewer.tsx
+++ src/features/collection/components/learning_units/FileViewer.tsx
@@ -1,10 +1,8 @@
-import styled from '@emotion/styled';
 import { ILeanScopeClient } from '@leanscope/api-client';
 import { LeanScopeClientContext } from '@leanscope/api-client/browser';
 import { Entity, EntityProps, useEntity } from '@leanscope/ecs-engine';
 import { Tags } from '@leanscope/ecs-models';
 import saveAs from 'file-saver';
-import { motion } from 'framer-motion';
 import { useContext, useEffect, useRef, useState } from 'react';
 import {
   IoArrowDownCircleOutline,
@@ -13,14 +11,12 @@ import {
   IoShareOutline,
   IoTrashOutline,
 } from 'react-icons/io5';
-import tw from 'twin.macro';
 import { FilePathFacet, FilePathProps, TitleProps } from '../../../../app/additionalFacets';
 import { AdditionalTag, DataType, SupabaseStorageBucket, SupabaseTable } from '../../../../base/enums';
-import { ActionRow, NavBarButton } from '../../../../components';
+import { ActionRow, NavBarButton, View } from '../../../../components';
 import { useIsViewVisible } from '../../../../hooks/useIsViewVisible';
 import supabaseClient from '../../../../lib/supabase';
 import { dataTypeQuery } from '../../../../utils/queries';
-import { useSelectedTheme } from '../../hooks/useSelectedTheme';
 
 const deleteFile = async (lsc: ILeanScopeClient, entity: Entity) => {
   const filePath = entity.get(FilePathFacet)?.props.filePath;
@@ -108,10 +104,9 @@ const FileViewer = (props: EntityProps & TitleProps & FilePathProps) => {
   const lsc = useContext(LeanScopeClientContext);
   const { entity, title, filePath } = props;
   const isVisible = useIsViewVisible(entity);
-  const { isDarkModeActive } = useSelectedTheme();
+
   const url = useFileUrl();
   const viewerRef = useRef<HTMLDivElement>(null);
-  const [isLoaded, setIsLoaded] = useState(false);
 
   const navigateBack = () => entity.add(AdditionalTag.NAVIGATE_BACK);
   const handleDownload = () => downloadFile(title, filePath);
@@ -119,9 +114,7 @@ const FileViewer = (props: EntityProps & TitleProps & FilePathProps) => {
 
   useOutsideClick(viewerRef, navigateBack);
 
-  const isFileViewerVisible = isVisible && isLoaded;
   const isDisplayedAsImage = title.endsWith('.png') || title.endsWith('.jpg') || title.endsWith('.jpeg');
-  const isDisplayedAsDocument = title.endsWith('.pdf') || title.endsWith('.docx');
 
   const handleShare = async () => {
     if (!url) return;
@@ -145,87 +138,47 @@ const FileViewer = (props: EntityProps & TitleProps & FilePathProps) => {
     }
   };
 
-  return filePath ? (
-    <div>
-      <Overlay
-        initial={{ opacity: 0 }}
-        animate={{
-          opacity: isVisible ? (isDarkModeActive ? 0.8 : 0.2) : 0,
-        }}
-        onClick={navigateBack}
-      />
-      {url && (
-        <div tw="flex  z-[600] justify-center items-center fixed w-screen h-screen">
-          <Viewer
-            isDisplayedAsDocument={isDisplayedAsDocument}
-            ref={viewerRef}
-            initial={{ scale: 0, opacity: 0, y: 600 }}
-            animate={{
-              scale: isFileViewerVisible ? 1 : 0,
-              opacity: isFileViewerVisible ? 1 : 0,
-              y: isFileViewerVisible ? 0 : 400,
-            }}
-            transition={{ type: 'tween', duration: 0.2 }}
+  return (
+    <View hidePadding visible={isVisible}>
+      <div tw="w-screen overflow-hidden h-screen">
+        <div tw="p-4 border-b border-primary-border dark:border-primary-border-dark w-screen xl:absolute bg-primary flex justify-between">
+          <div
+            tw="cursor-pointer text-2xl flex space-x-2 items-center dark:text-white text-primary-color"
+            onClick={navigateBack}
           >
-            <div tw="p-4 w-screen absolute bg-primary flex justify-between">
-              <div
-                tw="cursor-pointer text-2xl flex space-x-2 items-center dark:text-white text-primary-color"
-                onClick={navigateBack}
-              >
-                <IoChevronBack />
-                <p tw="text-sm">Zurück</p>
-              </div>
-              <div tw="flex h-fit space-x-4 lg:space-x-8 items-center ">
-                <NavBarButton onClick={handleShare}>
-                  <IoShareOutline />
-                </NavBarButton>
-                <NavBarButton
-                  content={
-                    <div>
-                      <ActionRow onClick={handleDownload} icon={<IoArrowDownCircleOutline />}>
-                        Herunterladen
-                      </ActionRow>
-                      <ActionRow last onClick={handleDelete} destructive icon={<IoTrashOutline />}>
-                        Löschen
-                      </ActionRow>
-                    </div>
-                  }
-                >
-                  <IoEllipsisHorizontalCircleOutline />
-                </NavBarButton>
-              </div>
-            </div>
-
-            <div tw="w-full h-full flex justify-center items-center ">
-              {isDisplayedAsImage && <img onLoad={() => setIsLoaded(true)} src={url} alt={title} />}
-
-              {title.endsWith('.pdf') && (
-                <iframe
-                  onLoad={() => setIsLoaded(true)}
-                  src={url}
-                  title={title}
-                  style={{ width: '100%', height: '100%' }}
-                />
-              )}
-            </div>
-          </Viewer>
+            <IoChevronBack />
+            <p tw="text-sm">Zurück</p>
+          </div>
+          <div tw="flex h-fit space-x-4 lg:space-x-8 items-center ">
+            <NavBarButton onClick={handleShare}>
+              <IoShareOutline />
+            </NavBarButton>
+            <NavBarButton
+              content={
+                <div>
+                  <ActionRow onClick={handleDownload} icon={<IoArrowDownCircleOutline />}>
+                    Herunterladen
+                  </ActionRow>
+                  <ActionRow last onClick={handleDelete} destructive icon={<IoTrashOutline />}>
+                    Löschen
+                  </ActionRow>
+                </div>
+              }
+            >
+              <IoEllipsisHorizontalCircleOutline />
+            </NavBarButton>
+          </div>
         </div>
-      )}
-    </div>
-  ) : null;
+
+        <div tw="w-full h-full bg-white flex justify-center items-center ">
+          {isDisplayedAsImage && <img src={url} alt={title} />}
+
+          {title.endsWith('.pdf') && <iframe src={url} title={title} style={{ width: '100%', height: '100%' }} />}
+        </div>
+      </div>
+    </View>
+  );
 };
-
-const Overlay = styled(motion.div)`
-  ${tw`  fixed z-[500] top-0 left-0 w-full h-full bg-black flex justify-center items-center`}
-`;
-
-const Viewer = styled(motion.div)<{ isDisplayedAsDocument?: boolean }>`
-  ${tw`  h-screen w-screen  bg-primary dark:bg-black`}
-
-  img {
-    ${tw`max-w-full max-h-full`}
-  }
-`;
 
 export default FileViewer;
 
