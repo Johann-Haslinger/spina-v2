a555c333dd2caec76183f86597cd5b1b98bab232
Johann Haslinger <heyjohn655@gmail.com>
2024-10-14 18:34:35 +0200

    chore: more scrolling improvements

--- src/components/presentation/View.tsx
+++ src/components/presentation/View.tsx
@@ -5,7 +5,7 @@ import tw from 'twin.macro';
 import { BackgroundOverlay } from '../../common/components/others';
 
 const StyledViewContainer = styled(motion.div)<{ backgroundColor?: string }>`
-  ${tw`fixed top-0 h-screen overflow-y-scroll left-0 w-screen backdrop-blur-2xl `}
+  ${tw`fixed top-0 h-screen overflow-y-scroll left-0 w-screen `}
   background-color: ${({ backgroundColor }) => backgroundColor};
   ${({ backgroundColor }) => !backgroundColor && tw` bg-primary dark:bg-primary-dark `}
 `;
@@ -21,7 +21,7 @@ const StyledViewContent = styled.div<{
   isOverlayView: boolean;
   hidePadding: boolean;
 }>`
-  ${tw` mx-auto overflow-y-hidden h-fit text-primary-text dark:text-primary-text-dark pb-60 md:pt-28 xl:pt-36 pt-16 w-full  px-4`}
+  ${tw` mx-auto overflow-y-hidden  text-primary-text dark:text-primary-text-dark md:pt-28 xl:pt-36 pt-16 w-full  px-4`}
   ${({ reducePaddingX: ignorePaddingX }) =>
     ignorePaddingX ? tw`md:w-[52rem] px-1.5` : tw` md:w-[48rem] xl:w-[51rem] 2xl:w-[56rem] `} 
   ${({ hidePadding }) => hidePadding && tw`!w-full !pt-0 px-0 !pb-0 `}
