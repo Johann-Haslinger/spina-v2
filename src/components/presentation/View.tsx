e14bda913604aad906a5314d5e055a822f2bed8c
Johann Haslinger <heyjohn655@gmail.com>
2024-10-14 18:37:49 +0200

    chore: add padding bottom to view component

--- src/components/presentation/View.tsx
+++ src/components/presentation/View.tsx
@@ -21,7 +21,7 @@ const StyledViewContent = styled.div<{
   isOverlayView: boolean;
   hidePadding: boolean;
 }>`
-  ${tw` mx-auto overflow-y-hidden  text-primary-text dark:text-primary-text-dark md:pt-28 xl:pt-36 pt-16 w-full  px-4`}
+  ${tw` mx-auto overflow-y-hidden pb-60 text-primary-text dark:text-primary-text-dark md:pt-28 xl:pt-36 pt-16 w-full  px-4`}
   ${({ reducePaddingX: ignorePaddingX }) =>
     ignorePaddingX ? tw`md:w-[52rem] px-1.5` : tw` md:w-[48rem] xl:w-[51rem] 2xl:w-[56rem] `} 
   ${({ hidePadding }) => hidePadding && tw`!w-full !pt-0 px-0 !pb-0 `}
