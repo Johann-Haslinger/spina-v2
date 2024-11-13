7853556e1e25fefdc10476b0b439355c002c0e00
Johann Haslinger <johann.hsr@icloud.com>
2024-11-13 20:33:17 +0100

    feat: add verified email page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,8 @@
-export { default as Overview } from './Overview';
-export { default as Homeworks } from './Homeworks';
-export { default as Exams } from './Exams';
 export { default as Collection } from './Collection';
-export { default as Groups } from './Groups';
+export { default as Exams } from './Exams';
 export { default as Flashcards } from './Flashcards';
+export { default as Groups } from './Groups';
+export { default as Homeworks } from './Homeworks';
+export { default as Overview } from './Overview';
 export { default as SuccessPage } from './SuccessPage';
+export { default as VerifiedEmailPage } from './VerifiedEmailPage';
