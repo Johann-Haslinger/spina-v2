361ce34b090081e1f1cc6a5421f2a3b27fd81c83
Johann Haslinger <johann.hsr@icloud.com>
2024-11-13 20:53:08 +0100

    chore: improve redirecting

--- src/features/tutorial/systems/TransitToTutorialSystem.tsx
+++ src/features/tutorial/systems/TransitToTutorialSystem.tsx
@@ -4,16 +4,19 @@ import { useLoadingIndicator } from '../../../common/hooks';
 import { useSchoolSubjectEntities } from '../../../common/hooks/useSchoolSubjects';
 import { Story } from '../../../common/types/enums';
 
+import { useLocation } from 'react-router-dom';
+
 const TransitToTutorialSystem = () => {
   const lsc = useContext(LeanScopeClientContext);
   const schoolSubjectEntities = useSchoolSubjectEntities();
   const { isLoadingIndicatorVisible } = useLoadingIndicator();
+  const location = useLocation();
 
   useEffect(() => {
-    if (schoolSubjectEntities.length > 0 || isLoadingIndicatorVisible) return;
+    if (schoolSubjectEntities.length > 0 || isLoadingIndicatorVisible || location.pathname === '/verified-email') return;
 
     lsc.stories.transitTo(Story.OBSERVING_TUTORIAL_STORY);
-  }, [schoolSubjectEntities.length, isLoadingIndicatorVisible]);
+  }, [schoolSubjectEntities.length, isLoadingIndicatorVisible, location.pathname]);
 
   return null;
 };
